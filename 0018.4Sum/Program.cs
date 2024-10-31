a140d61ef2fc032e0cc24333f3aa2eb0f8d15f2d
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-10-31 17:12:20 +0500

    0018.4Sum

---
+++ 0018.4Sum/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+    nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+ 
+
+    Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Example 2:
+
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+    -109 <= nums[i] <= 109
+    -109 <= target <= 109
+
+*/
+
+  // Code C# 
+public class Solution {
+    public IList<IList<int>> FourSum(int[] nums, int target) {
+        int n = nums.Length;
+        var ans = new List<IList<int>>();
+        if (n < 4) {
+            return ans;
+        }
+        Array.Sort(nums);
+        for (int i = 0; i < n - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int k = j + 1, l = n - 1;
+                while (k < l) {
+                    long x = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if (x < target) {
+                        ++k;
+                    } else if (x > target) {
+                        --l;
+                    } else {
+                        ans.Add(new List<int> {nums[i], nums[j], nums[k++], nums[l--]});
+                        while (k < l && nums[k] == nums[k - 1]) {
+                            ++k;
+                        }
+                        while (k < l && nums[l] == nums[l + 1]) {
+                            --l;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
