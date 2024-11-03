e36adb3c7547055c212596075a79418f5556bcda
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-11-04 01:18:30 +0500

    0027.Remove Element

---
+++ 0027.Remove Element/Program.cs
@@ -0,0 +1,27 @@
+﻿// C#
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int k = 0;
+        foreach (int x in nums) {
+            if (x != val) {
+                nums[k++] = x;
+            }
+        }
+        return k;
+    }
+}
+
+
+// Java
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int x : nums) {
+            if (x != val) {
+                nums[k++] = x;
+            }
+        }
+        return k;
+    }
+}
